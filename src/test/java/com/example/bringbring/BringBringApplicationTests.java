72b787357f73b2ce1e32ff465e739d75f82d4f29
Jongha Park <jongha2788@u.sogang.ac.kr>
2024-01-12 21:58:29 +0900

    feat: 프로젝트를 생성한다.

---
+++ src/test/java/com/example/bringbring/BringBringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bringbring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BringBringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
