df7ab011719f2ecbcfdfbb45889540b7c227593d
Jongha Park <jongha2788@u.sogang.ac.kr>
2024-01-13 02:17:22 +0900

    feat:cd 바꿔보기

--- src/main/java/com/example/bringbring/HelloController.java
+++ src/main/java/com/example/bringbring/HelloController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String hello(){
-        return "hello";
+        return "cicd 성공~~~";
     }
 }
